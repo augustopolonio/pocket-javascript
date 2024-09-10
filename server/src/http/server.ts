993ed476258815fe3bedbccb8884760109f02659
Augusto Polonio <augustodrp@gmail.com>
2024-09-09 21:25:56 -0300

    chore: fastify basic config

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -1,1 +1,10 @@
-console.log('Hello, World!')
+import fastify from "fastify";
+
+const app = fastify();
+
+app.listen(
+  { 
+    port: 3333 
+  }).then(() => {
+    console.log('🌐 HTTP Server running on http://localhost:3333')
+  })
