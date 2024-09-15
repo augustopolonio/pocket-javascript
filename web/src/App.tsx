5d841874f355aca72d86ad5f80aed01816fe3d42
Augusto Polonio <augustodrp@gmail.com>
2024-09-14 22:21:17 -0300

    feat(ui): import components to use and install necessary packages

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,6 +1,7 @@
 import logo from './assets/logo-in-orbit.svg'
 import letsStart from './assets/lets-start.svg'
 import { Plus } from 'lucide-react'
+import { Button } from './components/ui/button'
 
 export function App() {
   return (
@@ -10,13 +11,11 @@ export function App() {
       <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
         Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
       </p>
-      <button
-        type="button"
-        className="px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600"
-      >
+
+      <Button>
         <Plus />
         Cadastrar meta
-      </button>
+      </Button>
     </div>
   )
 }
