bd499978a7a7d4e69b235718fb1e7c1b22747f8c
Augusto Polonio <augustodrp@gmail.com>
2024-09-15 20:51:43 -0300

    feat: get week summary from database using TanStack Query

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,35 @@
 import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
-import { EmptyGoals } from './components/empty-goals'
 import { Summary } from './components/summary'
+import { EmptyGoals } from './components/empty-goals'
+import { useQuery } from '@tanstack/react-query'
+
+type SummaryResponse = {
+  completed: number
+  total: number
+  goalsPerDay: Record<
+    string,
+    {
+      id: number
+      title: string
+      completedAt: Date
+    }[]
+  >
+}
 
 export function App() {
+  const { data } = useQuery<SummaryResponse>({
+    queryKey: ['summary'],
+    queryFn: async () => {
+      const response = await fetch('http://localhost:3333/summary')
+      const data = await response.json()
+      return data.summary
+    },
+  })
+
   return (
     <Dialog>
-      {/* <EmptyGoals /> */}
-      <Summary />
+      {data && data?.total > 0 ? <Summary /> : <EmptyGoals />}
       <CreateGoal />
     </Dialog>
   )
